e99152412dca27c0710935204c3fdd41548741c7
thaovyvan <thaovy.j.van@gmail.com>
2019-12-04 03:08:17 -0500

    hello

--- app/src/main/java/com/example/piggybank_app/welcomeActivity.java
+++ app/src/main/java/com/example/piggybank_app/welcomeActivity.java
@@ -2,14 +2,23 @@ package com.example.piggybank_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class welcomeActivity extends AppCompatActivity {
+    public static String welcomeBackName;
+    TextView welcomeBackText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        welcomeBackText = (TextView)findViewById(R.id.welcome_textView);
+        Intent intent = getIntent();
+        welcomeBackName = intent.getStringExtra("username");
+        welcomeBackText.setText("Welcome back " + welcomeBackName);
     }
 }
