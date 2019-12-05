1e80508f4a5bde418290660a3e8c29f772a0b372
Carl Vann <carlvann16@gmail.com>
2019-12-05 11:40:41 -0500

    Added Stripe
    Need to figure out how to connect to a user's bank account and pull recent transactions

--- app/src/main/java/com/example/piggybank_app/profileActivity.java
+++ app/src/main/java/com/example/piggybank_app/profileActivity.java
@@ -10,12 +10,19 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.stripe.android.CustomerSession;
+import com.stripe.android.PaymentConfiguration;
+import com.stripe.android.Stripe;
+import com.stripe.android.model.Customer;
+
 public class profileActivity extends AppCompatActivity {
 
     TextView spending, username;
     ImageView profile_picture;
     RecyclerView recent_transactions;
     Button back;
+    Stripe stripe = new Stripe(this, "pk_test_UVYfBYe81Mtx9O192lqNO1IW00EAI3P22t");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +36,16 @@ public class profileActivity extends AppCompatActivity {
         recent_transactions = (RecyclerView) findViewById(R.id.recent_transactions_view);
         back = (Button) findViewById(R.id.back_profile);
 
+        PaymentConfiguration.init(getApplicationContext(), "pk_test_UVYfBYe81Mtx9O192lqNO1IW00EAI3P22t");
+
     }
 
     private void setUsername(String str){
         //should probably moved into onCreate once the app is further along
         username.setText(str);
+    }
+
+    private void getTransactionHistory(){
     }
 
 
