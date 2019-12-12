e75ad5785fab0febeba3f20029de25650f1add3b
thaovyvan <thaovy.j.van@gmail.com>
2019-12-12 01:41:08 -0500

    temp

--- app/src/main/java/com/example/piggybank_app/welcomeActivity.java
+++ app/src/main/java/com/example/piggybank_app/welcomeActivity.java
@@ -2,6 +2,8 @@ package com.example.piggybank_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlarmManager;
 import android.app.NotificationChannel;
@@ -16,6 +18,16 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class welcomeActivity extends AppCompatActivity {
@@ -27,6 +39,17 @@ public class welcomeActivity extends AppCompatActivity {
 
     private String CHANNEL_ID = "PiggyBank";
 
+    JSONObject object;
+    JSONObject getObject;
+    JSONArray getArray;
+
+    Object obj;
+
+    ArrayList<String> transactions;
+
+    MyRecyclerViewAdapter adapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,11 +59,45 @@ public class welcomeActivity extends AppCompatActivity {
         welcomeBackName = intent.getStringExtra("username");
         welcomeBackText.setText("Welcome back " + welcomeBackName);
 
+        JSONParser parser = new JSONParser();
+
+        try {
+            obj = parser.parse(new FileReader("transactions-json.json"));
+            //object = new JSONObject("transactions-json.json");
+            //getObject = object.getJSONObject("Transactions");
+            //getArray = getObject.getJSONArray("TransactionsArray");
+
+            getArray = (JSONArray) obj;
+            transactions = getTransactions(getArray);
+        } catch (Exception e) {}
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.welcomeRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, transactions);
+        recyclerView.setAdapter(adapter);
+
+
 
     }
 
     public void goToSettings(View view) {
         Intent intent = new Intent(this, settingsActivity.class);
         startActivity(intent);
+    }
+
+    public static ArrayList<String> getTransactions(JSONArray jsonArray) throws JSONException {
+
+        ArrayList<String> returnList = new ArrayList<String>();
+        for (int i=0; i<jsonArray.length(); i++) {
+            JSONObject currObject = jsonArray.getJSONObject(i);
+            String transactionName = currObject.getString("name");
+            String transactionCost = currObject.getString("cost");
+
+            String finalString = String.format("%s, %s", transactionName, transactionCost);
+            returnList.add(finalString);
+        }
+
+        return returnList;
     }
 }
